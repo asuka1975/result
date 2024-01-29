678be616c36f6d16d78eb738087be1dc4b10f95e
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2024-01-30 00:35:10 +0900

    feat: declare methods

--- include/result/result.hpp
+++ include/result/result.hpp
@@ -1,6 +1,7 @@
 #ifndef RESULT_RESULT_HPP
 #define RESULT_RESULT_HPP
 
+#include <functional>
 #include <variant>
 
 template <class Left, class Right>
@@ -8,6 +9,16 @@ class Result {
 public:
     Result();
     Result(std::variant<Left, Right> valueOrError);
+    template <class RightOut>
+    Result<Left, RightOut> map(std::function<RightOut(Right)> func);
+    template <class RightOut>
+    Result<Left, RightOut> flatMap(std::function<Result<Left, Right>(Right)> func);
+    template <class Visitor>
+    auto visit(Visitor visitor);
+    Right get() const;
+    Right getOr(Right defaultValue) const noexcept;
+    Right getOr(std::function<Right()> supplier) const noexcept;
+    bool hasValue() const noexcept;
 
 private:
     std::variant<Left, Right> valueOrError;
