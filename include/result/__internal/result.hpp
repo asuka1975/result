09d80b56459d92161f767b01ca0ae23425ce37fe
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2024-01-30 21:48:13 +0900

    feat: implement all methods

--- include/result/__internal/result.hpp
+++ include/result/__internal/result.hpp
@@ -1,6 +1,8 @@
 #ifndef RESULT___INTERNAL_RESULT_HPP
 #define RESULT___INTERNAL_RESULT_HPP
 
+#include "visitor_utils.hpp"
+
 template <class Left, class Right>
 inline Result<Left, Right>::Result() : Right(Right{}) {}
 
@@ -10,39 +12,47 @@ inline Result<Left, Right>::Result(std::variant<Left, Right> valueOrError) : val
 template <class Left, class Right>
 template <class RightOut>
 inline Result<Left, RightOut> Result<Left, Right>::map(std::function<RightOut(Right)> func) {
-
+    return std::visit(internal::visitor_utils::MapVisitor<Left, Right, RightOut>{ std::move(func) }, valueOrError);
 }
 
 template <class Left, class Right>
 template <class RightOut>
 inline Result<Left, RightOut> Result<Left, Right>::flatMap(std::function<Result<Left, RightOut>(Right)> func) {
-
+    return std::visit(internal::visitor_utils::FlatMapVisitor<Left, Right, RightOut>{ std::move(func) }, valueOrError);
 }
 
 template <class Left, class Right>
 template <class Visitor>
 inline auto Result<Left, Right>::visit(Visitor visitor) -> decltype(visitor(std::declval<Left>())) {
-
+    return std::visit(visitor, valueOrError);
 }
 
 template <class Left, class Right>
 inline Right Result<Left, Right>::get() const {
-
+    return std::get<1>(valueOrError);
 }
 
 template <class Left, class Right>
 inline Right Result<Left, Right>::getOr(Right defaultValue) const noexcept {
-
+    auto p = std::get<1>(valueOrError);
+    if(!p) {
+        return defaultValue;
+    }
+    return *p;
 }
 
 template <class Left, class Right>
 inline Right Result<Left, Right>::getOr(std::function<Right()> supplier) const {
-
+    auto p = std::get_if<1>(&valueOrError);
+    if(!p) {
+        return supplier();
+    }
+    return *p;
 }
 
 template <class Left, class Right>
 inline bool Result<Left, Right>::hasValue() const noexcept {
-
+    return std::get_if<1>(&valueOrError) != nullptr;
 }
 
 #endif
---
+++ include/result/__internal/visitor_utils.hpp
@@ -0,0 +1,38 @@
+#ifndef RESULT___INTERNAL_VISITOR_UTILS_HPP
+#define RESULT___INTERNAL_VISITOR_UTILS_HPP
+
+#include <functional>
+#include <variant>
+namespace internal::visitor_utils {
+    template <class Left, class Right, class RightOut> 
+    class MapVisitor {
+    public:
+
+        MapVisitor(std::function<RightOut(Right)> func) : func(std::move(func)) {}
+        std::variant<Left, RightOut> operator()(Left l) {
+            return std::variant<Left, RightOut>(std::move(l));
+        }
+        std::variant<Left, RightOut> operator()(Right r) {
+            return std::variant<Left, RightOut>(func(std::move(r)));
+        }
+    private:
+        std::function<RightOut(Right)> func;
+    };
+
+    template <class Left, class Right, class RightOut> 
+    class FlatMapVisitor {
+    public:
+
+        FlatMapVisitor(std::function<Result<Left, RightOut>(Right)> func) : func(std::move(func)) {}
+        std::variant<Left, RightOut> operator()(Left l) {
+            return std::variant<Left, RightOut>(std::move(l));
+        }
+        std::variant<Left, RightOut> operator()(Right r) {
+            return func(std::move(r));
+        }
+    private:
+        std::function<Result<Left, RightOut>(Right)> func;
+    };
+}
+
+#endif
