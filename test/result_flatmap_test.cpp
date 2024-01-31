58ad571a6eb3dc66080e5fb436dde865ac490ebc
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2024-01-31 18:40:34 +0900

    feat: add test for flatmap

--- test/result_flatmap_test.cpp
+++ test/result_flatmap_test.cpp
@@ -25,8 +25,30 @@ TEST(MapTest, NormalCase) {
     EXPECT_EQ(actual, 7);
 }
 
-TEST(MapTest, ErrorCase) {
+TEST(MapTest, AlreadyErrorReturnNormalCase) {
     Error actual = div10(0)
+        .flatMap<int>([](int n) {
+            return asuka1975::Result<Error, int> { n + 2};
+        })
+        .getError();
+    
+    EXPECT_EQ(actual.errorCode, 1);
+    EXPECT_EQ(actual.message, "cannot divide by zero");
+}
+
+TEST(MapTest, AlreadyErrorReturnErrorCase) {
+    Error actual = div10(0)
+        .flatMap<int>([](int n) {
+            return asuka1975::Result<Error, int>(Error { 1, "error" });
+        })
+        .getError();
+    
+    EXPECT_EQ(actual.errorCode, 1);
+    EXPECT_EQ(actual.message, "cannot divide by zero");
+}
+
+TEST(MapTest, ReturnErrorCase) {
+    Error actual = div10(2)
         .flatMap<int>([](int n) {
             return asuka1975::Result<Error, int>(Error { 1, "error" });
         })
